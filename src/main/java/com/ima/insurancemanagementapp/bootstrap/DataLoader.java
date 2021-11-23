d9f93358a0ab1da4cdf328c39c7fbbd206600c27
Konrad <iryczek40@gmail.com>
2021-11-23 12:00:04 +0100

    Small package changes

--- src/main/java/com/ima/insurancemanagementapp/bootstrap/DataLoader.java
+++ src/main/java/com/ima/insurancemanagementapp/bootstrap/DataLoader.java
@@ -18,11 +18,11 @@ public class DataLoader implements ApplicationRunner {
     }
 
     public void run(ApplicationArguments args) {
-        clientRepository.save(new Client("Konrad", "Ryczek", "moj@email.com", LocalDate.now()));
+        clientRepository.save(new Client("Konrad", "Ryczek", "moj@email.com", LocalDate.now().minusDays(1)));
         clientRepository.save(new Client("Mariusz", "Perek", "mariusz@email.com", LocalDate.now()));
-        clientRepository.save(new Client("Ilona", "Mik", "mik@email.com", LocalDate.now()));
-        clientRepository.save(new Client("Peter", "Johannes", "peterek@email.com", LocalDate.now()));
-        clientRepository.save(new Client("Iwona", "Pawlik", "iwona@email.com", LocalDate.now()));
+        clientRepository.save(new Client("Ilona", "Mik", "mik@email.com", LocalDate.now().minusDays(15)));
+        clientRepository.save(new Client("Peter", "Johannes", "peterek@email.com", LocalDate.now().minusMonths(22)));
+        clientRepository.save(new Client("Iwona", "Pawlik", "iwona@email.com", LocalDate.now().minusWeeks(33)));
     }
 }
 
